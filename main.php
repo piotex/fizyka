cfbd630e8ea32b5f8874c312b32241739996c865
Piotr2\Piotr <pkubon2@gmail.com>
2019-05-28 18:32:36 +0200

    navi

---
+++ main.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <link rel="stylesheet" type="text/css" href="style/main.css">
+        <link rel="stylesheet" type="text/css" href="style/mobile.css">
+        <link rel="stylesheet" type="text/css" href="style/pc.css">
+    </head>
+<body>
+    <header>
+        <div class="header_logo"><img src="images/logo.png" alt="Zdjęcie niedostępne."></div>
+        <div class="header_button_1"></div>
+    </header>
+    <div class="header_space"></div>
+    <div class="container">
+        <div class="content">
+            <div class="main_content_text_2">Pytania</div>
+            <div class="main_content_text_1">Odpowiedzi</div>
+            <div class="main_content_text_2">Rozwiązania</div>
+            <div class="main_content_text_3">Z dziedziny matematyki, fizyki i języka</div>
+
+        </div>
+        <div class="navi">
+            <div class="navi_title">Fizyka</div> 
+            <a href="">
+                <div class="navi_fiz_menu">Ruch postępowy</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Siła</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Praca, Moc, Energia</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Hydrostatyka</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Grawitacja</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Bryła sztywna</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Fale</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Termodynamika</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Elektrostatyka</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Prąd stały</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Elektromagnetyzm</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Optyka</div> 
+            </a>
+            <a href="">
+                <div class="navi_fiz_menu">Dualizm</div> 
+            </a>
+        </div>
+    </div>
+    <footer>
+        <b>Kontakt:</b> 
+        <p>E-mail: pkubon2@gmail.com</p>
+    </footer>
+    <script src="scripts/main.js"></script>
+</body>
+</html>
