bbaff3db8dd6f778841635937444bf418c1e3036
Piotr2\Piotr <pkubon2@gmail.com>
2019-08-21 10:36:51 +0200

    css

--- layouts/Start.php
+++ layouts/Start.php
@@ -2,3 +2,4 @@
 <div class="main_content_text_1">Odpowiedzi</div>
 <div class="main_content_text_2">Rozwiązania</div>
 <div class="main_content_text_3">Z dziedziny matematyki, fizyki i języka</div>
+<div class="main_content_text_3">Jeśli masz jakieś zadania którymi chciał byś się podzielić, podeślij je wraz z pytaniami na maila.</div>
